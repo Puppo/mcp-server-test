f1ea51aba55784bacb28a170263dbc46a0749741
Luca Del Puppo <Puppo@users.noreply.github.com>
2026-01-09 02:27:11 +0100

    feat: add eslint for code linting and fix scripts in package.json

---
+++ eslint.config.ts
@@ -0,0 +1,13 @@
+import js from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
+  js.configs.recommended,
+  tseslint.configs.strict,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+    languageOptions: { globals: globals.node }
+  },
+]);
